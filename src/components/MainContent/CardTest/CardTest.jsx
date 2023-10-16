db73784e3a46288a7abfc553e72b951a1f7c0dcd
Sergey Pokusaev <work.gooner1886@gmail.com>
2023-10-16 22:08:56 +0400

    edit json

--- src/components/MainContent/CardTest/CardTest.jsx
+++ src/components/MainContent/CardTest/CardTest.jsx
@@ -2,32 +2,23 @@ import React from "react";
 import style from "./CardTest.module.css";
 const CardTest = (props) => {
   return (
-    
-      <div class={style.item}>
-        <div class={style.desc}>
-          <div class={style.desc__prop}>
-            <p class={style.desc__title}>
-              {props.item.title}
-            </p>
-            <p
-              class={style.desc__title_prop}
-              
-            >
-              {props.item.descr}
-            </p>
-          </div>
-          <div class={style.desc__price}>
-            <p class={style.desc__price_num}>{props.item.price}₽</p>
-            <div class={style.desc__price_addToCart}>
-              <button class={style.desc__price_button}>+</button>
-            </div>
-          </div>
+    <div class={style.item}>
+      <div class={style.desc}>
+        <div class={style.desc__prop}>
+          <h2 class={style.desc__title}>{props.item.title}</h2>
+          <p class={style.desc__title_prop}>{props.item.descr}</p>
         </div>
-        <div class={style.images}>
-          <img src={"./images/menu" + props.item.gallery} alt="" />
+        <div class={style.desc__price}>
+          <p class={style.desc__price_num}>{props.item.price}₽</p>
+          <div class={style.desc__price_addToCart}>
+            <button class={style.desc__price_button}>+</button>
+          </div>
         </div>
       </div>
-    
+      <div class={style.images}>
+        <img src={"./images/menu" + props.item.gallery} alt="" />
+      </div>
+    </div>
   );
 };
 
