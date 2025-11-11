4b0d276989c1f0439b061800c2da6f8f3b0453f2
Sergey Pokusaev <work.gooner1886@gmail.com>
2025-11-11 20:56:44 +0400

    тестовая версия, для обновления

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,124 +1,214 @@
-import React, { useContext } from 'react';
+// import React, { useContext } from 'react';
+// import style from './Menu.module.css';
+// import Context from '../../Context/Context';
+// import MenuItem from './MenuItem/MenuItem';
+// import Category from './Category/Category';
+// import { Route, Routes } from 'react-router-dom';
+// import Soup from './Category/Soup/Soup';
+// import HotDish from './Category/HotDish/HotDish';
+// import Poke from './Category/Poke/Poke';
+// import Snacks from './Category/Snacks/Snacks';
+// import Pastes from './Category/Pastes/Pastes';
+// import Lemonades from './Category/Lemonades/Lemonades';
+// import Tea from './Category/Tea/Tea';
+// import Coffee from './Category/Coffee/Coffee';
+// import Desserts from './Category/Desserts/Desserts';
+// import Milkshake from './Category/Milkshake/Milkshake';
+// import Smoothie from './Category/Smoothie/Smoothie';
+// import Salads from './Category/Salads/Salads';
+// import ModalMenu from './ModalMenu/ModalMenu';
+// import NonAlco from './Category/NonAlco/NonAlco';
+// import Alco from './Category/Alco/Alco';
+// import Coctail from './Category/Coctail/Coctail';
+
+// const Menu = () => {
+//   const { products, products_aroma, order, addToOrder, removeFromOrder, item, setItem, switchMenuMode } =
+//     useContext(Context);
+
+//   const closeModalForEdit = () => {
+//     setItem(null);
+//   };
+
+//   return (
+//     <div className={style.wrapper}>
+//       <div>
+//         <Category />
+//         <Routes>
+//           <Route path={'/salads'} element={<Salads openModalForEdit={setItem} />}></Route>
+//           <Route path={'/soup'} element={<Soup openModalForEdit={setItem} />}></Route>
+//           <Route path={'/HotDish'} element={<HotDish openModalForEdit={setItem} />}></Route>
+//           <Route path={'/poke'} element={<Poke openModalForEdit={setItem} />}></Route>
+//           <Route path={'/snacks'} element={<Snacks openModalForEdit={setItem} />}></Route>
+//           <Route path={'/pastes'} element={<Pastes openModalForEdit={setItem} />}></Route>
+//           <Route path={'/lemonades'} element={<Lemonades openModalForEdit={setItem} />}></Route>
+//           <Route path={'/tea'} element={<Tea openModalForEdit={setItem} />}></Route>
+//           <Route path={'/coffee'} element={<Coffee openModalForEdit={setItem} />}></Route>
+//           <Route path={'/desserts'} element={<Desserts openModalForEdit={setItem} />}></Route>
+//           <Route path={'/milkshake'} element={<Milkshake openModalForEdit={setItem} />}></Route>
+//           <Route path={'/smoothie'} element={<Smoothie openModalForEdit={setItem} />}></Route>
+//           <Route path={'/nonalco'} element={<NonAlco openModalForEdit={setItem} />}></Route>
+//           <Route path={'/alco'} element={<Alco openModalForEdit={setItem} />}></Route>
+//           <Route path={'/coctail'} element={<Coctail openModalForEdit={setItem} />}></Route>
+//         </Routes>
+//         <div className={style.item__wrapper}>
+//           <Routes>
+//             <Route
+//               path={'/'}
+//               element={
+//                 switchMenuMode === false
+//                   ? products_aroma.map((item) => {
+//                       let portion;
+//                       if (item.editions) {
+//                         portion = item.editions.map((el) => order[el.uid] || 0).reduce((sum, port) => sum + port, 0);
+//                       } else {
+//                         portion = order[item.uid];
+//                       }
+//                       return (
+//                         <MenuItem
+//                           key={item.uid}
+//                           item={item}
+//                           portion={portion || 0}
+//                           addToOrder={addToOrder}
+//                           removeFromOrder={removeFromOrder}
+//                           openModalForEdit={setItem}
+//                         />
+//                       );
+//                     })
+//                   : products.map((item) => {
+//                       let portion;
+//                       if (item.editions) {
+//                         portion = item.editions.map((el) => order[el.uid] || 0).reduce((sum, port) => sum + port, 0);
+//                       } else {
+//                         portion = order[item.uid];
+//                       }
+
+//                       return (
+//                         <MenuItem
+//                           key={item.uid}
+//                           item={item}
+//                           portion={portion || 0}
+//                           addToOrder={addToOrder}
+//                           removeFromOrder={removeFromOrder}
+//                           openModalForEdit={setItem}
+//                         />
+//                       );
+//                     })
+//               }
+//             ></Route>
+//           </Routes>
+//           {item && (
+//             <ModalMenu
+//               addToOrder={addToOrder}
+//               item={item}
+//               closeModalForEdit={closeModalForEdit}
+//               portion={order[item.uid] || 0}
+//             />
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Menu;
+// /*
+// "UID salads (1-99), UID pasta (100-199),
+// UID lightSnacks (200-299), UID poke (300-399),
+// UID hotDish (400-499), UID soup (500-599),
+// UID snacks (600-699), UID desserts (700-799),
+// UID iceCream (800-899), UID milkshake (900-999),
+// UID smoothie (1000-1099), UID coffee (1100-1199),
+// UID tea (1200-1299), UID lemonade (1300-1399),
+// UID drink (1400-1499), UID alcohol (1500-1599),
+// UID options (1600-1699)",
+//  */
+import React, { useContext, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 import style from './Menu.module.css';
 import Context from '../../Context/Context';
 import MenuItem from './MenuItem/MenuItem';
 import Category from './Category/Category';
-import { Route, Routes } from 'react-router-dom';
-import Soup from './Category/Soup/Soup';
-import HotDish from './Category/HotDish/HotDish';
-import Poke from './Category/Poke/Poke';
-import Snacks from './Category/Snacks/Snacks';
-import Pastes from './Category/Pastes/Pastes';
-import Lemonades from './Category/Lemonades/Lemonades';
-import Tea from './Category/Tea/Tea';
-import Coffee from './Category/Coffee/Coffee';
-import Desserts from './Category/Desserts/Desserts';
-import Milkshake from './Category/Milkshake/Milkshake';
-import Smoothie from './Category/Smoothie/Smoothie';
-import Salads from './Category/Salads/Salads';
 import ModalMenu from './ModalMenu/ModalMenu';
-import NonAlco from './Category/NonAlco/NonAlco';
-import Alco from './Category/Alco/Alco';
-import Coctail from './Category/Coctail/Coctail';
+
+const MemoizedMenuItem = React.memo(MenuItem);
+
+const CATEGORY_TITLES = {
+  salads: 'Салаты',
+  pasta: 'Пасты',
+  snacks: 'Закуски',
+  poke: 'Поке',
+  hotDish: 'Горячее',
+  soup: 'Супы',
+  desserts: 'Десерты',
+  iceCream: 'Мороженое',
+  milkshake: 'Молочные коктейли',
+  smoothie: 'Смузи',
+  coffee: 'Кофе',
+  tea: 'Чай',
+  lemonade: 'Лимонады',
+  drink: 'Напитки безалкогольные',
+  alcohol: 'Напитки алкогольные',
+  coctail: 'Коктейли',
+};
 
 const Menu = () => {
-  const { products, products_aroma, order, addToOrder, removeFromOrder, item, setItem, switchMenuMode } =
-    useContext(Context);
+  const { currentProducts, order, addToOrder, removeFromOrder, item, setItem } = useContext(Context);
+  const { '*': currentCategory } = useParams(); // "salads", "poke", null
 
-  const closeModalForEdit = () => {
-    setItem(null);
-  };
+  const groupedItems = useMemo(() => {
+    const groups = {};
+    currentProducts.forEach((item) => {
+      let cat = item.category || 'other';
+      if (cat === 'lightSnacks') {
+        cat = 'snacks';
+      }
+      if (!groups[cat]) groups[cat] = [];
+      groups[cat].push(item);
+    });
+    return groups;
+  }, [currentProducts]);
+
+  // Что показывать
+  const itemsToShow = currentCategory ? groupedItems[currentCategory] || [] : currentProducts;
+
+  const renderedItems = useMemo(() => {
+    return itemsToShow.map((item) => {
+      const portion = item.editions
+        ? item.editions.reduce((sum, ed) => sum + (order[ed.uid] || 0), 0)
+        : order[item.uid] || 0;
+
+      return (
+        <MemoizedMenuItem
+          key={item.uid}
+          item={item}
+          portion={portion}
+          addToOrder={addToOrder}
+          removeFromOrder={removeFromOrder}
+          openModalForEdit={setItem}
+        />
+      );
+    });
+  }, [itemsToShow, order, addToOrder, removeFromOrder, setItem]);
+
+  const categoryTitle = currentCategory ? CATEGORY_TITLES[currentCategory] : null;
 
   return (
     <div className={style.wrapper}>
-      <div>
-        <Category />
-        <Routes>
-          <Route path={'/salads'} element={<Salads openModalForEdit={setItem} />}></Route>
-          <Route path={'/soup'} element={<Soup openModalForEdit={setItem} />}></Route>
-          <Route path={'/HotDish'} element={<HotDish openModalForEdit={setItem} />}></Route>
-          <Route path={'/poke'} element={<Poke openModalForEdit={setItem} />}></Route>
-          <Route path={'/snacks'} element={<Snacks openModalForEdit={setItem} />}></Route>
-          <Route path={'/pastes'} element={<Pastes openModalForEdit={setItem} />}></Route>
-          <Route path={'/lemonades'} element={<Lemonades openModalForEdit={setItem} />}></Route>
-          <Route path={'/tea'} element={<Tea openModalForEdit={setItem} />}></Route>
-          <Route path={'/coffee'} element={<Coffee openModalForEdit={setItem} />}></Route>
-          <Route path={'/desserts'} element={<Desserts openModalForEdit={setItem} />}></Route>
-          <Route path={'/milkshake'} element={<Milkshake openModalForEdit={setItem} />}></Route>
-          <Route path={'/smoothie'} element={<Smoothie openModalForEdit={setItem} />}></Route>
-          <Route path={'/nonalco'} element={<NonAlco openModalForEdit={setItem} />}></Route>
-          <Route path={'/alco'} element={<Alco openModalForEdit={setItem} />}></Route>
-          <Route path={'/coctail'} element={<Coctail openModalForEdit={setItem} />}></Route>
-        </Routes>
-        <div className={style.item__wrapper}>
-          <Routes>
-            <Route
-              path={'/'}
-              element={
-                switchMenuMode === false
-                  ? products_aroma.map((item) => {
-                      let portion;
-                      if (item.editions) {
-                        portion = item.editions.map((el) => order[el.uid] || 0).reduce((sum, port) => sum + port, 0);
-                      } else {
-                        portion = order[item.uid];
-                      }
-                      return (
-                        <MenuItem
-                          key={item.uid}
-                          item={item}
-                          portion={portion || 0}
-                          addToOrder={addToOrder}
-                          removeFromOrder={removeFromOrder}
-                          openModalForEdit={setItem}
-                        />
-                      );
-                    })
-                  : products.map((item) => {
-                      let portion;
-                      if (item.editions) {
-                        portion = item.editions.map((el) => order[el.uid] || 0).reduce((sum, port) => sum + port, 0);
-                      } else {
-                        portion = order[item.uid];
-                      }
-
-                      return (
-                        <MenuItem
-                          key={item.uid}
-                          item={item}
-                          portion={portion || 0}
-                          addToOrder={addToOrder}
-                          removeFromOrder={removeFromOrder}
-                          openModalForEdit={setItem}
-                        />
-                      );
-                    })
-              }
-            ></Route>
-          </Routes>
-          {item && (
-            <ModalMenu
-              addToOrder={addToOrder}
-              item={item}
-              closeModalForEdit={closeModalForEdit}
-              portion={order[item.uid] || 0}
-            />
-          )}
-        </div>
+      <Category />
+      <div className={style.item__wrapper}>
+        {categoryTitle && <h2 className={style.categoryTitle}>{categoryTitle}</h2>}
+        {renderedItems.length > 0 ? renderedItems : <p>Товары не найдены</p>}
+        {item && (
+          <ModalMenu
+            item={item}
+            closeModalForEdit={() => setItem(null)}
+            portion={order[item.uid] || 0}
+            addToOrder={addToOrder}
+          />
+        )}
       </div>
     </div>
   );
 };
 
 export default Menu;
-/*
-"UID salads (1-99), UID pasta (100-199),
-UID lightSnacks (200-299), UID poke (300-399),
-UID hotDish (400-499), UID soup (500-599),
-UID snacks (600-699), UID desserts (700-799),
-UID iceCream (800-899), UID milkshake (900-999),
-UID smoothie (1000-1099), UID coffee (1100-1199),
-UID tea (1200-1299), UID lemonade (1300-1399),
-UID drink (1400-1499), UID alcohol (1500-1599),
-UID options (1600-1699)",
- */
