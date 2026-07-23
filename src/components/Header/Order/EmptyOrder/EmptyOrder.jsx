462ab97b4a445fa43b8d651eb54be078117d5465
Sergey Pokusaev <work.gooner1886@gmail.com>
2026-07-23 20:31:39 +0400

    fix css

--- src/components/Header/Order/EmptyOrder/EmptyOrder.jsx
+++ src/components/Header/Order/EmptyOrder/EmptyOrder.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import style from './EmptyOrder.module.css';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const EmptyOrder = ({ branch, branchName }) => {
   return (
     <div className={style.empty}>
       <p>Ваша корзина пуста</p>
-      <NavLink to={`/${branch}/menu`} className={style.empty__link_button}>
+      <Link to={`/${branch}/menu`} className={style.empty__link_button}>
         <button className={style.empty_button}>меню {branchName}</button>
-      </NavLink>
+      </Link>
     </div>
   );
 };
